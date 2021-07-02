be5288d5172c9b6a817b498bbee62e9947600eef
pj-web <insta.pj.05@gmail.com>
2021-07-02 16:53:47 +0300

    before uploading to hosting

--- dist/js/today.js
+++
@@ -1,8 +0,0 @@
-function today() {
-    d=new Date();
-    p=new Date(d.getTime());
-    monthA="января,февраля,марта,апреля,мая,июня,июля,августа,сентября,октября,ноября,декабря".split(",");
-    document.write(p.getDate()+" "+monthA[p.getMonth()]+" "+p.getFullYear()) + " г. ";
-}
-
-console.log(today());
